f35923fb2a332b9c43dad9ddb8da45f67d3b46ee
Eatmoonpie <ochsmobile@gmail.com>
2025-11-20 13:23:17 -0500

    end card and back to title

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneLoader : MonoBehaviour
+{
+   
+    public void GoToTitleScreen()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+}
